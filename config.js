f1252135f4475169d21b21a8d5c94ad61ab62837
TSMSHAFTY <konstantinasenov1997@gmail.com>
2022-02-15 18:16:30 +0100

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports ={
+  MONGODB: 'mongodb+srv://eroxthayeeter:UbV5lXCZHbcBI8eh@cluster0.ydas2.mongodb.net/exfnchat?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
+
+
+
+
+
+
+// eroxthayeeter | UbV5lXCZHbcBI8eh
+
+// mongodb
